f561ac7341d95e6df1389066b69f476c42a7089e
Grockkk <dela.kontakt@gmail.com>
2023-04-03 23:35:34 +0200

    some minor changes with add delete and update

--- Blazor_Application/Client/Sevices/ClientService/ClientService.cs
+++ Blazor_Application/Client/Sevices/ClientService/ClientService.cs
@@ -1,4 +1,5 @@
 ﻿using Blazor_Application.Shared;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Net.Http.Json;
 
@@ -8,9 +9,11 @@ namespace Blazor_Application.Client.Sevices.ClientService
     {
 
         private readonly HttpClient _http;
+        private readonly NavigationManager _navigationManager;
 
-        public ClientService(HttpClient http) {
+        public ClientService(HttpClient http, NavigationManager navigationManager) {
             _http = http;
+            _navigationManager = navigationManager;
         }
 
         public List<client> Clients { get; set; } = new List<client>();
@@ -18,13 +21,13 @@ namespace Blazor_Application.Client.Sevices.ClientService
         public async Task CreateClient(client client)
         {
             var outcome = await _http.PostAsJsonAsync($"api/client", client);
-            UpdateList(outcome);
+            await UpdateList(outcome);
         }
 
         public async Task DeleteClient(int VAT_ID_number)
         {
             var outcome = await _http.DeleteAsync($"api/client/byTax?id={VAT_ID_number}");
-            UpdateList(outcome);
+            await UpdateList(outcome);
         }
 
         public async Task GetClients()
@@ -50,13 +53,15 @@ namespace Blazor_Application.Client.Sevices.ClientService
 
         public async Task UpdateClient(client client)
         {
-            var outcome = await _http.PutAsJsonAsync($"api/client/byTax?id={client.VAT_ID_number}", client);
-            UpdateList(outcome);
+            var outcome = await _http.PutAsJsonAsync($"api/client/{client.VAT_ID_number}", client);
+            await UpdateList(outcome);
         }
+
         public async Task UpdateList(HttpResponseMessage outcome)
         {
             var updatedList = await outcome.Content.ReadFromJsonAsync<List<client>>();
             Clients = updatedList;
+            _navigationManager.NavigateTo("client");
         }
 
     }
